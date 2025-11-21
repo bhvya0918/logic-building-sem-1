59bdfe2c872e90d834ceeaf06f3c57aecf826adf
bhvya0918 <bhvyapalecha0918@gmail.com>
2025-11-21 10:31:12 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   hello.c

---
+++ hello.c
@@ -0,0 +1,70 @@
+/*
+#include<stdio.h>
+// this code dispalys how we print any thing                 or
+
+/*
+we can write this type 
+also if we need in para 
+
+// printing age using cases in output (integer me )     %d
+int main(){
+    int age =44;
+    printf("age is %d",age);
+    return 0;
+              
+
+}
+
+//printing character using character %C
+
+#include <stdio.h>
+int mmain(){
+    int float pi =3.1414;
+    printf("charcter is %f",pi); 
+    return 0;
+}
+
+
+
+// printing charct %c
+
+#include <stdio.h>
+int main(){
+    char hastage='#';
+    printf("character is %c",hastage);
+    return 0;
+
+}
+
+
+
+
+
+
+#include<stdio.h>
+int main ()
+{
+    char hastage='#';
+    printf("char is %c",hastage);
+    return 0;
+}
+
+ #include <stdio.h>
+ int main(){
+    char star='*';
+    printf("character is %c", star);
+    return 0;
+
+ }
+
+ 
+*/
+#include <stdio.h>
+int main(){
+    int age;
+    printf("enter your age");
+    scanf("%d",&age);
+    printf("your age is %d",age);
+    return 0;
+}
+
